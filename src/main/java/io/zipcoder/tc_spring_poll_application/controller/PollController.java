16305dd45d02db0b5d018ba5ea8f9b3cdfdd41a0
mike <mike.schoenbeck@gmail.com>
2019-12-07 17:22:40 -0500

    completed 5.2

--- src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
@@ -1,6 +1,7 @@
 package io.zipcoder.tc_spring_poll_application.controller;
 
 import io.zipcoder.tc_spring_poll_application.domain.Poll;
+import io.zipcoder.tc_spring_poll_application.exception.ResourceNotFoundException;
 import io.zipcoder.tc_spring_poll_application.repositories.PollRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 
 import java.net.URI;
 
@@ -42,21 +44,29 @@ public class PollController {
 
     @RequestMapping(value="/polls/{pollId}", method=RequestMethod.GET)
     public ResponseEntity<?> getPoll(@PathVariable Long pollId) {
-        Poll p = pollRepository.findOne(pollId);
-        return new ResponseEntity<> (p, HttpStatus.OK);
+        verifyPoll(pollId);
+        return new ResponseEntity<> (pollRepository.findOne(pollId), HttpStatus.OK);
     }
 
     @RequestMapping(value="/polls/{pollId}", method=RequestMethod.PUT)
     public ResponseEntity<?> updatePoll(@RequestBody Poll poll, @PathVariable Long pollId) {
-        // Save the entity
+        verifyPoll(pollId);
         Poll p = pollRepository.save(poll);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @RequestMapping(value="/polls/{pollId}", method=RequestMethod.DELETE)
     public ResponseEntity<?> deletePoll(@PathVariable Long pollId) {
+        verifyPoll(pollId);
         pollRepository.delete(pollId);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    public void verifyPoll(Long pollId)    {
+       Poll poll = pollRepository.findOne(pollId);
+       if(poll == null) {
+           throw new ResourceNotFoundException("Poll ID " + pollId + " not found.");
+        }
     }
 
 
