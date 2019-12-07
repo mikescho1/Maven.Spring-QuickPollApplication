218193af1233ac0bf567df859777743db2bcd9e4
mike <mike.schoenbeck@gmail.com>
2019-12-07 13:33:18 -0500

    added delete method to poll controller

--- src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -42,6 +39,27 @@ public class PollController {
         httpHeaders.setLocation(newPollUri);
         return new ResponseEntity<>(httpHeaders, HttpStatus.CREATED);
     }
+
+    @RequestMapping(value="/polls/{pollId}", method=RequestMethod.GET)
+    public ResponseEntity<?> getPoll(@PathVariable Long pollId) {
+        Poll p = pollRepository.findOne(pollId);
+        return new ResponseEntity<> (p, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/polls/{pollId}", method=RequestMethod.PUT)
+    public ResponseEntity<?> updatePoll(@RequestBody Poll poll, @PathVariable Long pollId) {
+        // Save the entity
+        Poll p = pollRepository.save(poll);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/polls/{pollId}", method=RequestMethod.DELETE)
+    public ResponseEntity<?> deletePoll(@PathVariable Long pollId) {
+        pollRepository.delete(pollId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
 
 
 
