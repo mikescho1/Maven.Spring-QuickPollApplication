4a5a588637f4788f8e8197742da2a5e64b6c39bf
mike <mike.schoenbeck@gmail.com>
2019-12-07 20:25:24 -0500

    created dummy poll data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,74 @@
+insert into poll (poll_id, question) values (1, 'What is your favorite animal?');
+insert into option (option_id, option_value, poll_id) values (1, 'Dog', 1);
+insert into option (option_id, option_value, poll_id) values (2, 'Cat', 1);
+insert into option (option_id, option_value, poll_id) values (3, 'Hamster', 1);
+
+insert into poll (poll_id, question) values (2, 'What is your favorite color?');
+insert into option (option_id, option_value, poll_id) values (4, 'Red', 2);
+insert into option (option_id, option_value, poll_id) values (5, 'Blue', 2);
+insert into option (option_id, option_value, poll_id) values (6, 'Green', 2);
+
+insert into poll (poll_id, question) values (3, 'What is your favorite subject?');
+insert into option (option_id, option_value, poll_id) values (7, 'History', 3);
+insert into option (option_id, option_value, poll_id) values (8, 'Math', 3);
+insert into option (option_id, option_value, poll_id) values (9, 'Science', 3);
+
+insert into poll (poll_id, question) values (4, 'What is your favorite meal?');
+insert into option (option_id, option_value, poll_id) values (10, 'Breakfast', 4);
+insert into option (option_id, option_value, poll_id) values (11, 'Lunch', 4);
+insert into option (option_id, option_value, poll_id) values (12, 'Dinner', 4);
+
+insert into poll (poll_id, question) values (5, 'What is your favorite kind of music?');
+insert into option (option_id, option_value, poll_id) values (13, 'Rock', 5);
+insert into option (option_id, option_value, poll_id) values (14, 'Country', 5);
+insert into option (option_id, option_value, poll_id) values (15, 'Rap', 5);
+
+insert into poll (poll_id, question) values (6, 'What is your favorite sport?');
+insert into option (option_id, option_value, poll_id) values (16, 'Golf', 6;
+insert into option (option_id, option_value, poll_id) values (17, 'Soccer', 6);
+insert into option (option_id, option_value, poll_id) values (18, 'Football', 6);
+
+insert into poll (poll_id, question) values (7, 'What is your favorite food?');
+insert into option (option_id, option_value, poll_id) values (19, 'Pizza', 7);
+insert into option (option_id, option_value, poll_id) values (20, 'Burger', 7);
+insert into option (option_id, option_value, poll_id) values (21, 'Apple', 7);
+
+insert into poll (poll_id, question) values (8, 'What is your favorite soda?');
+insert into option (option_id, option_value, poll_id) values (22, 'Pepsi', 8);
+insert into option (option_id, option_value, poll_id) values (23, 'Coke', 8);
+insert into option (option_id, option_value, poll_id) values (24, 'Sprite', 8);
+
+insert into poll (poll_id, question) values (9, 'What is your favorite beer?');
+insert into option (option_id, option_value, poll_id) values (25, 'Light', 9);
+insert into option (option_id, option_value, poll_id) values (26, 'Lager', 9);
+insert into option (option_id, option_value, poll_id) values (27, 'IPA', 9);
+
+insert into poll (poll_id, question) values (10, 'What is your favorite movie?');
+insert into option (option_id, option_value, poll_id) values (28, 'Batman', 10);
+insert into option (option_id, option_value, poll_id) values (29, 'Spiderman', 10);
+insert into option (option_id, option_value, poll_id) values (30, 'Superman', 10);
+
+insert into poll (poll_id, question) values (11, 'What is your favorite state?');
+insert into option (option_id, option_value, poll_id) values (31, 'Hawaii', 11);
+insert into option (option_id, option_value, poll_id) values (32, 'Delaware', 11);
+insert into option (option_id, option_value, poll_id) values (33, 'Maine', 11);
+
+insert into poll (poll_id, question) values (12, 'What is your favorite oceanl?');
+insert into option (option_id, option_value, poll_id) values (34, 'Atlantic', 12);
+insert into option (option_id, option_value, poll_id) values (35, 'Pacific', 12);
+insert into option (option_id, option_value, poll_id) values (36, 'Indian', 12);
+
+insert into poll (poll_id, question) values (13, 'What is your favorite season?');
+insert into option (option_id, option_value, poll_id) values (37, 'Spring', 13);
+insert into option (option_id, option_value, poll_id) values (38, 'Summer', 13);
+insert into option (option_id, option_value, poll_id) values (39, 'Fall', 13);
+
+insert into poll (poll_id, question) values (14, 'What is your favorite car?');
+insert into option (option_id, option_value, poll_id) values (40, 'Ford', 14);
+insert into option (option_id, option_value, poll_id) values (41, 'Chevy', 14);
+insert into option (option_id, option_value, poll_id) values (42, 'Toyota', 14);
+
+insert into poll (poll_id, question) values (15, 'What is your favorite OS?');
+insert into option (option_id, option_value, poll_id) values (43, 'Mac', 15);
+insert into option (option_id, option_value, poll_id) values (44, 'Windows', 15);
+insert into option (option_id, option_value, poll_id) values (45, 'Linux', 15);
